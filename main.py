84412c98ffeaf3dee6bc604ad006244029b3bf0f
Niiko235 <nicolaseplaza@gmail.com>
2026-03-21 16:13:33 -0500

    add: implement data generation and plotting for 2D point clouds in main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('hola mundo')
+from generarDatos import generar_datos_2d_2Nubes
+
+from graficas import single_nube_2d
+
+
+x_nube1, y_nube1 = generar_datos_2d_2Nubes()
+
+print(x_nube1)
+print(y_nube1)
+
+single_nube_2d(x_nube1, y_nube1, 'ro')
+
+
+
+
